8a798b4b1fa60d3a5e826683979fb87b72f0fb4c
Mbote-Joseph <mbotejoseph001@gmail.com>
2023-02-05 21:33:46 +0300

    NodeJS Recap

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", { title: "Welcome, Joseph Mbote" });
 });
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Us" });
+});
